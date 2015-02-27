8370a80c9d8addc55e616afd84a8b1df84ec4cef
Kazuki Ohara <kazuki.ohara@gmail.com>
2015-02-27 12:45:22 +0900

    add 17_texture_wrapmode

---
+++ opengl/17_texture_wrapmode/texture_wrapmode.cpp
@@ -0,0 +1,140 @@
+// original code: https://github.com/openglsuperbible/sb6code/blob/master/src/wrapmodes/wrapmodes.cpp
+/*
+* Copyright © 2012-2013 Graham Sellers
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice (including the next
+* paragraph) shall be included in all copies or substantial portions of the
+* Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <sb6ktx.h>
+
+GLuint rendering_program;
+GLuint vertex_array_object;
+GLuint offset_location;
+GLuint texture;
+
+GLuint compile_shaders(void)
+{
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+	GLuint program;
+
+	static const GLchar *vertex_shader_source[] = {
+		"#version 430 core														\n"
+		"																		\n"
+		"uniform vec2 offset;													\n"
+		"																		\n"
+		"out vec2 tex_coord;													\n"
+		"																		\n"
+		"void main(void)														\n"
+		"{																		\n"
+		"	const vec4 vertices[] = vec4[](vec4(-0.45f, -0.45f, 0.5f, 1.0f),	\n"
+		"								   vec4( 0.45f, -0.45f, 0.5f, 1.0f),	\n"
+		"								   vec4(-0.45f,  0.45f, 0.5f, 1.0f),	\n"
+		"								   vec4( 0.45f,  0.45f, 0.5f, 1.0f));	\n"
+		"																		\n"
+		"	gl_Position = vertices[gl_VertexID] + vec4(offset, 0.0f, 0.0f);		\n"
+		"	tex_coord = vertices[gl_VertexID].xy * 3.0f + vec2(0.45f * 3);		\n"
+		"}																		\n"
+	};
+
+	static const GLchar *fragment_shader_source[] = {
+		"#version 430 core														\n"
+		"																		\n"
+		"uniform sampler2D s;													\n"
+		"																		\n"
+		"out vec4 color;														\n"
+		"																		\n"
+		"in vec2 tex_coord;														\n"
+		"																		\n"
+		"void main(void)														\n"
+		"{																		\n"
+		"	color = texture(s, tex_coord);										\n"
+		"}																		\n"
+	};
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, vertex_shader_source, nullptr);
+	glCompileShader(vertex_shader);
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, fragment_shader_source, nullptr);
+	glCompileShader(fragment_shader);
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	return program;
+}
+
+void startup(void)
+{
+	rendering_program = compile_shaders();
+
+	offset_location = glGetUniformLocation(rendering_program, "offset");
+
+	texture = sb6::ktx::file::load("../media/textures/rightarrows.ktx");
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glGenVertexArrays(1, &vertex_array_object);
+	glBindVertexArray(vertex_array_object);
+}
+
+void render(double currentTime)
+{
+	static const GLfloat green[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+	static const GLfloat yellow[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+	glClearBufferfv(GL_COLOR, 0, green);
+
+	static const GLenum wrapmodes[] = { GL_CLAMP_TO_EDGE, GL_REPEAT, GL_CLAMP_TO_BORDER, GL_MIRRORED_REPEAT };
+	static const GLfloat offsets[] = { -0.5f, -0.5f,
+										0.5f, -0.5f,
+									   -0.5f,  0.5f,
+										0.5f,  0.5f };
+
+	glUseProgram(rendering_program);
+	glViewport(0, 0, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, yellow);
+
+	for (int i = 0; i < 4; i++) {
+		glUniform2fv(offset_location, 1, &offsets[i * 2]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapmodes[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapmodes[i]);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	}
+}
+
+void on_resize(int width, int height)
+{
+}
+
+void shutdown(void)
+{
+	glDeleteVertexArrays(1, &vertex_array_object);
+	glDeleteProgram(rendering_program);
+}
